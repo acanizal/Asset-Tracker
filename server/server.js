46860eb79c8a7e6f8268a9ec14d6c178a96bedc2
acanizal <alex.canizal.d@gmail.com>
2025-02-03 21:58:31 -0600

    Set up express.json() middleware, set up requests to test with Postman

--- server/server.js
+++ server/server.js
@@ -2,13 +2,20 @@
 //dotenv package for managing environment variables, port...
 require("dotenv").config();
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+
+//express middleware for retrieving data from the client - in JSON format to 
+//takes the information in the body of the request and sets it equal to teh req object
+app.use(express.json());
+
 
 //get all assets
 app.get("/api/v1/assets", (req, res) => {
-    //http://localhost:4000/api/v1/getAssets this is the route that this get api will use
+    //http://localhost:4000/api/v1/assets this is the route that this get api will use
 
+
+    //responses back to the client
     res.status(200).json({
         status: "success",
         data: {
@@ -20,9 +27,47 @@ app.get("/api/v1/assets", (req, res) => {
 //get one asset
 app.get("/api/v1/assets/:id", (req, res) => {
     console.log(req);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            asset: "LT-qwerty"
+        }
+    })
 });
 
+//creat an asset
+app.post("/api/v1/assets", (req, res) => {
+        console.log(req.body);
 
+        res.status(201).json({
+            status: "success",
+            data: {
+                asset: "LT-qwerty"
+            }
+        })
+});
+
+//update an asset
+app.put("/api/v1/assets/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            asset: "LT-qwerty"
+        }
+    })
+});
+
+//delete an asset
+
+app.delete("/api/v1/assets/:id", (req, res) => {
+    res.status(204).json({
+        status: "success",
+    })
+});
 
 //env variable for PORT in .env file, if it is not available, use 3001
 const port = process.env.PORT || 3001;
